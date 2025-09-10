506c777040a1ce16807bc17efa9d29b66ec30454
Alexander Guerrero <alexander.guerrero@ucb.edu.bo>
2025-09-10 19:29:34 -0400

    ejercicio 4

---
+++ c/tareas/05_ejercicio/inc/src/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    int numero, suma = 0;
+
+    printf("Ingresa un número entero: ");
+    scanf("%d", &numero);
+
+    for (int i = 1; i <= numero; i++) {
+        suma += i;
+    }
+    
+    return 0;
+}
